34c15609a2d14caa3809694e3505e0334e1020fe
Volodumur712 <citrusdolar@gmail.com>
2022-10-27 19:13:13 +0300

    probel

--- script.js
+++ script.js
@@ -50,19 +50,27 @@ gamer = document.querySelector("#player");
 
 // упрвління кнопками вверх вниз
 document.onkeydown = function moveGamer(event) {
-	//console.dir(event);
+	console.dir(event);
 	// рух вверх
 	if(event.keyCode == 87 || event.keyCode == 38) {
 		if(gamer.offsetTop > 75) {
 			gamer.style.top = gamer.offsetTop - 20 + "px";
 		}
 	}
+
 	// вниз рух
 	if(event.keyCode == 83 || event.keyCode == 40) {
 		if(gamer.offsetTop <  440) {
 			gamer.style.top = gamer.offsetTop + 20 + "px";
 		}	
 	}
+
+	// вистріл пробелом
+	probel = 32;
+	if (event.keyCode == probel) {
+		createBullet();
+	}
+
 }
 
 // функція старт гри
@@ -71,49 +79,94 @@ function startGame() {
 	blocGame.style.display = "block";
 
 // енемі перший
-	enemy1 = document.querySelector(".enemy.type-1");
-	moveEnemy1(enemy1);
+	createEnemy1();
 
 // другий
-	enemy2 = document.querySelector(".enemy.type-2");
-	moveEnemy2(enemy2);
+	createEnemy2();
 
 // куля
-	bullet1 = document.querySelector(".bullet");
-	moveBullet(bullet1);
+	createBullet();
 }
 
+
+/* Робота з ворогами
+ // Зробити створення кулі при натисканні на пробіл. 
+ // Коли куля долітає до кінця поля, видаляти кулю і зупиняти таймер кулі.  є  */ 
+
+// стартова позиція енемі
+position1 = 100;
+position2 = 400;
+
+ // створення елементу
+function createEnemy1() {
+	let enemy = document.createElement("div");
+	enemy.className = "enemy type-1";
+	// зміна позиції наступного
+	enemy.style.top = position1 + "px";
+	position1 = position1 + 40;
+
+	// розміщення енемі на екрані і щоб рухався
+	blocGame.appendChild(enemy);
+	moveEnemy1(enemy);
+}
+
+function createEnemy2() {
+	let enemy = document.createElement("div");
+	enemy.className = "enemy type-2";
+	// зміна позиції наступного
+	enemy.style.top = position2 + "px";
+	position2 = position2 - 50;
+
+	// розміщення енемі на екрані і щоб рухався
+	blocGame.appendChild(enemy);
+	moveEnemy2(enemy);
+}
+ 
 // рух енемі 1
 function moveEnemy1(enemy) {
 	//console.dir(enemy);
-	setInterval(function() {
+	let timerID = setInterval(function() {
 		enemy.style.left = enemy.offsetLeft - 10 + "px";
 		//console.dir(enemy.offsetLeft);
-		if(enemy.offsetLeft < -100) {
-			enemy.style.left = document.querySelector("body").clientWidth + 150 + "px";
+		if (enemy.offsetLeft < -100) {
+			// якщо позиція менше 100 видаляємо ремувом
+			enemy.remove();
+			// і створюється новий
+			createEnemy1();
+			// зупинка інтервалу
+			clearInterval(timerID);
 		}
 	}, 50);
 }
 
 // рух енемі 2
 function moveEnemy2(enemy) {
-	setInterval(function() {
+	let timerID = setInterval(function() {
 		enemy.style.left = enemy.offsetLeft - 8 + "px";
 		if(enemy.offsetLeft < -100) {
-			enemy.style.left = document.querySelector("body").clientWidth + 150 + "px";
+			enemy.remove();
+			createEnemy2();
+			clearInterval(timerID);
 		}
 	}, 70);
+}
+
+// вистріл
+// створення кулі
+function createBullet() {
+	let bullet1 = document.createElement("div");
+	bullet1.className = "bullet";
+	blocGame.appendChild(bullet1);
+	moveBullet(bullet1);
 }
 
 // куля рух
 function moveBullet(bullet) {
 	//console.dir(bullet);
-	setInterval(function() {
+	let timerID = setInterval(function() {
 		bullet.style.left = bullet.offsetLeft + 30 + "px";
 		if(bullet.offsetLeft > 1500) {
-			bullet.style.left = document.querySelector("body").clientWidth -1099 + "px";
+			bullet.remove();
 		}
 	}, 30);
 }
-
- 
